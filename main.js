9244f1ce5805c2f82288e5d054517faa1fdd47a1
peri <peri@1password.com>
2021-02-06 20:25:27 -0800

    no message

--- main.js
+++ main.js
@@ -3,22 +3,23 @@ document.querySelector('button').addEventListener('click', getPwned)
 function getPwned(){
 //    headers: {'hibp-api-key': '97c4020a628f4b07b57dd60b194e77a3'}
 //});
- 'origin: http://localhost:8888'
 
   const email = document.querySelector('input').value
-  const url = "https://haveibeenpwned.com/api/v3/breachedaccount/" +encodeURIComponent(email)
+  const url = "https://cors-anywhere.herokuapp.com/https://haveibeenpwned.com/api/v3/breachedaccount/" +encodeURIComponent(email)
   fetch(url, {
     method: "GET",
     withCredentials: true,
     headers: {
-      'hibp-api-key': '97c4020a628f4b07b57dd60b194e77a3'
+      'hibp-api-key': '97c4020a628f4b07b57dd60b194e77a3',
+      'user-agent': 'Peri Ahmadi'
 }
 })
     .then (res => res.json())
     .then (data => {
-      document.querySelector('h2').innerText = data.title
+      document.querySelector('p').innerText = data[].Name
+      console.log(data);
     })
     .catch(err => {
-    console.log('error ${err}');
+    console.log(`error ${err}`);
 })
 }
